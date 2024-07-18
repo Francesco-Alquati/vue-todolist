45ee63d43be3c1eae4e7c3021866d906fafe73ae
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-19 00:03:36 +0200

    [UPD] Created array todo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,52 @@
+const todo = [
+    {
+        id: 1,
+        Text:'ciao',
+        done: false
+    },
+    {
+        id: 2,
+        Text:'ciao',
+        done: false
+    },
+    {
+        id: 3,
+        Text:'ciao',
+        done: true
+    },
+    {
+        id: 4,
+        Text:'ciao',
+        done: false
+    },
+    {
+        id: 5,
+        Text:'ciao',
+        done: false
+    },
+    {
+        id: 6,
+        Text:'ciao',
+        done: true
+    },
+    {
+        id: 7,
+        Text:'ciao',
+        done: true
+    }
+]
 
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            todolist: todo,
+        }
+    },
+    methods: {
+
+    }
+}).mount('#app')
 
